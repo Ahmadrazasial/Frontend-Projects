e32cf790f2674bafb4ef91b3bcb3420d9c18e896
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-05-11 22:23:47 +0500

    new added

--- Quiz app/quiz.js
+++ Quiz app/quiz.js
@@ -6,23 +6,64 @@ let sawal = document.querySelectorAll(".sawal");
 let submit = document.querySelectorAll(".Check");
 let next = document.querySelectorAll(".next");
 let options = document.querySelectorAll(".options")
-let selected = document.querySelector('input[name="options"]:checked');
+let radios = document.querySelector('input[name="options"]');
 // let label = selected.closest('label');
 const answers = ["Ramzan"];
 
-startBtn.addEventListener("click",()=>{
-info.style.display = "none";
-qBox.style.display = "block";
-startBtn.style.display = "none"
-// qBox.style.opacity = 1;
-// qBox.style.transition= "opacity .5s ease-in-out";
+startBtn.addEventListener("click", () => {
+    info.style.display = "none";
+    qBox.style.display = "block";
+    startBtn.style.display = "none"
+    // qBox.style.opacity = 1;
+    // qBox.style.transition= "opacity .5s ease-in-out";
 })
-submit.forEach((btn,index) =>{
-btn.addEventListener("click",(e)=>{
-    e.preventDefault();
-    next[index].style.display = "inline-block"
-    if(!selected){
-        alert("select")
+submit.forEach((btn, index) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const currentS = Array.from(sawal).find(q => {
+            return window.getComputedStyle(q).display !== "none";
+        })
+
+        const selected = currentS.querySelector('input[name="options"]:checked');
+
+        if (!selected) {
+            alert("Please select an option");
+        }
+        else if (selected) {
+            submit[index].style.display = "none"
+            next[index].style.display = "inline-block";
+            const label = selected.closest("label");
+
+            const text = label.textContent.trim();
+            if (answers.includes(text)) {
+                label.style.color = "Green";
+            } else {
+                label.style.color = "red";
+                const radios = currentS.querySelector('input[name="options"]');
+                const otherlabel = radios.closest("label");
+                const otherText = otherlabel.textContent.trim();
+                if (answers.includes(otherText)) {
+                    otherlabel.style.color = "green";
+                }
+            }
+        }
+    })
+})
+function nextSawal() {
+    const currentS = Array.from(sawal).find(q => {
+        return window.getComputedStyle(q).display !== "none";
+    })
+    const nextS = currentS.nextElementSibling;
+    if (nextS) {
+        currentS.style.display = "none";
+        nextS.style.display = "block";
     }
-})
+
+}
+next.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        nextSawal();
+    })
 })
