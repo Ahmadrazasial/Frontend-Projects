80c89e72e821f5574ed68ae6869c63b3bfd8cc78
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-05-23 23:04:00 +0500

    updated

--- Image gallery/script.js
+++ Image gallery/script.js
@@ -111,27 +111,49 @@ list.addEventListener("keydown", (e) => {
 function zoom() {
     let zoomed = true;
     let currentY = 0;
+    function length(e,element){
+        
+        const rect = mainImg.getBoundingClientRect();
+        // alert(rect.left);
+   
+        const offsetX = e.clientX - rect.left;
+        
+        const offsetY = e.clientY - rect.top;
+        // alert(offsetX & offsetY);
+
+        const percentX = (offsetX / rect.width) * 100;
+        const percentY = (offsetY / rect.height) * 100;
+
+        return {percentX ,percentY};
+    }
     mainImg.addEventListener("dblclick", (e) => {
         // alert("clicked")
+         const {percentX,percentY} = length(e,mainImg);
 
         if (!zoomed) {
 
-            let height = preview.clientHeight;
-            console.log(preview);
-            const newHeight = height * 2;
-
-
+            mainImg.style.transformOrigin = `${percentX}% ${percentY}%`;
             mainImg.style.transform = "scale(2)";
             preview.style.overflow = "hidden";
-            // mainImg.style.cursor = "zoom-out";
             zoomed = true;
         } else if (zoomed) {
+            mainImg.style.transformOrigin = " center center";
             mainImg.style.transform = "scale(1)";
             zoomed = false;
             preview.style.height = '';
-            //   mainImg.style.cursor = "zoom-out";
+           
         }
     })
+
+        mainImg.addEventListener("wheel",(e)=>{
+            const {percentX , percentY} = length(e,mainImg)
+            if(e.ctrlKey){
+                mainImg.style.transformOrigin = `${percentX}% ${percentY}%`;
+                mainImg.style.transform = "scale(2)";
+                zoomed = true;
+            }
+        })
+
 
     mainImg.setAttribute('tabindex', '0');
     mainImg.focus();
@@ -171,9 +193,75 @@ zoom();
 let add = document.querySelector(".add");
 let thumbnailBtn = document.querySelector("#thumbnail");
   let addBox = document.querySelector(".adding");
-add.addEventListener("click",()=>{
-    addBox.style.display = "block";
+  let rotated = false;
+
+add.addEventListener("click",(e)=>{
+    e.stopPropagation();
+    rotated = !rotated;
+
+    add.style.transform = rotated ? "rotate(45deg)": "rotate(0deg)";
+    addBox.classList.toggle("show");
 })
-// document.addEventListener("click",()=>{
-//     addBox.style.display = "none";
+addBox.addEventListener("click",(e)=>{
+    e.stopPropagation();
+})
+document.body.addEventListener("click",()=>{
+    addBox.classList.remove("show");
+    add.style.transform = "rotate(0deg)"
+})
+
+let imgAdd = document.querySelector("#cam");
+let folAdd = document.querySelector("#fol");
+
+folAdd.addEventListener("click", () => folderInput.click());
+imgAdd.addEventListener("click", () => {
+    input.click();
+})
+
+
+
+ 
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+
+right.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let listImg = document.getElementsByClassName("smallImg");
+    let array = Array.from(listImg);
+   const currentIndex = array.findIndex(img => img.src === mainImg.src)
+    if(currentIndex !== -1 && currentIndex < array.length - 1){
+        const nextImg = array[currentIndex + 1];
+        mainImg.src = nextImg.src ;
+    }
+})
+left.addEventListener("click",(e)=>{
+    let listImg = document.getElementsByClassName("smallImg");
+    let array = Array.from(listImg);
+    const currentIndex = array.findIndex(img => img.src === mainImg.src);
+    if(currentIndex > 0){
+        const preImg = array[currentIndex - 1];
+        mainImg.src = preImg.src;
+    }
+})
+
+// mainImg.addEventListener("mouseenter",()=>{
+//     isHovered = true;
+//     if(e.key === "ArrowRight"){
+//         right.click();
+//     }
 // })
+
+document.body.addEventListener("keydown",(e)=>{
+    if(e.ctrlKey && e.key === "i"){
+        input.click();
+    }
+    if(e.ctrlKey && e.key === "f"){
+        folderInput.click();
+    }
+    if(e.key === "ArrowRight"){
+        right.click();
+    }
+    if(e.key === "ArrowLeft"){
+        left.click();
+    }
+})
