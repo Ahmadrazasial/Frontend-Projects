744acbc6fcc89c81331c772d1c4d90dc19711633
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-05-25 21:44:51 +0500

    updated

--- Image gallery/script.js
+++ Image gallery/script.js
@@ -1,4 +1,3 @@
-// const { createElement } = require("react");
 
 console.log("Pinned");
 let upload = document.getElementById("upload");
@@ -6,6 +5,8 @@ let input = document.getElementById("uploadFile");
 let mainImg = document.getElementById("uploadedImg");
 let preview = document.querySelector(".view");
 let uploadBtn = document.querySelector(".viewBtn");
+
+/*uploading single image reading the file and converting it as a url for image src */
 
 upload.addEventListener("click", () => {
     input.click();
@@ -27,13 +28,16 @@ input.addEventListener("change", function () {
     }
 })
 
+/** uploading a complete folder */
+
 let folder = document.getElementById("folder");
 let thumbnails = document.getElementById("thumbnails");
 let folderBtn = document.getElementById("folderBtn");
 let folderInput = document.querySelector("#folUpload");
 let list = document.querySelector("#list");
-folderBtn.addEventListener("click", () => folderInput.click());
+folderBtn.addEventListener("click", () => folderInput.click());//clicks the folder input
 
+//function when flder is uploaded
 folderInput.addEventListener("change", function (e) {
     e.preventDefault();
     const files = this.files;
@@ -69,76 +73,86 @@ folderInput.addEventListener("change", function (e) {
             reader.readAsDataURL(file);
         })
     }
+
+    //makes small image appear in large
+    // let zoomed = false;
     function changeImg() {
         let img = document.querySelectorAll(".smallImg");
+        
         img.forEach(image => {
             image.addEventListener("click", () => {
                 mainImg.src = image.src
-
             });
         })
     }
     changeImg();
-
 })
+
+//scrolls small images on mouse wheel
 list.addEventListener("wheel", (e) => {
     e.preventDefault();
     list.scrollLeft += e.deltaY;
 })
 
+
+//to scroll left on click
 let next = document.querySelector("#next")
-next.addEventListener("click",(e)=>{
+next.addEventListener("click", (e) => {
     e.preventDefault();
-    back.style.display = "block"
+    back.style.opacity = 1;
+    back.style.visibility = "visible";
     list.scrollBy({
-        left:+200,
-        behavior:'smooth',
+        left: +200,
+        behavior: 'smooth',
     })
     const max = list.scrollWidth - list.clientWidth;
-     if(list.scrollLeft >= max ){
-        next.style.display = "none";
+    if (list.scrollLeft >= max) {
+        next.style.opacity = 0;
+        next.style.visibility = "hidden";
     }
 })
 
+//scroll right on click
 let back = document.querySelector("#back");
-back.addEventListener("click",(e)=>{
-    next.style.display = "block";
-     list.scrollBy({
-             left: -200,
-             behavior: 'smooth',
-        })
-        if(list.scrollLeft <= 0){
-            back.style.display = "none";
-        }
+back.addEventListener("click", (e) => {
+    next.style.opacity = 1;
+    next.style.visibility = "visible";
+    list.scrollBy({
+        left: -200,
+        behavior: 'smooth',
+    })
+    if (list.scrollLeft <= 0) {
+        back.style.opacity = 0;
+        back.style.visibility = "hidden";
+    }
 })
-    
-    
 
+//zooms the large image
 function zoom() {
     let zoomed = true;
     let currentY = 0;
-    function length(e,element){
-        
-        const rect = mainImg.getBoundingClientRect();
+    function length(e, element) {
+
+        const rect = mainImg.getBoundingClientRect();//gets image lengths
         // alert(rect.left);
-   
-        const offsetX = e.clientX - rect.left;
-        
-        const offsetY = e.clientY - rect.top;
+
+        const offsetX = e.clientX - rect.left; //cursor from left
+
+        const offsetY = e.clientY - rect.top;//from top
         // alert(offsetX & offsetY);
 
-        const percentX = (offsetX / rect.width) * 100;
+        const percentX = (offsetX / rect.width) * 100; //cursor position in percent
         const percentY = (offsetY / rect.height) * 100;
 
-        return {percentX ,percentY};
+        return { percentX, percentY };
     }
     mainImg.addEventListener("dblclick", (e) => {
         // alert("clicked")
-         const {percentX,percentY} = length(e,mainImg);
+        const { percentX, percentY } = length(e, mainImg);//on function call gets percent values frommain image and stores in local variab;les
 
         if (!zoomed) {
 
-            mainImg.style.transformOrigin = `${percentX}% ${percentY}%`;
+            mainImg.style.transformOrigin = `${percentX}% ${percentY}%`;//where to zoom
             mainImg.style.transform = "scale(2)";
             preview.style.overflow = "hidden";
             zoomed = true;
@@ -147,31 +161,31 @@ function zoom() {
             mainImg.style.transform = "scale(1)";
             zoomed = false;
             preview.style.height = '';
-           
+
         }
     })
 
-
+    //slides image up and down when zoomed.Zoom is working but limits re not
     mainImg.setAttribute('tabindex', '0');
     mainImg.focus();
     mainImg.addEventListener("keydown", (e) => {
 
         if (!zoomed) return;
-        const preBox = preview.getBoundingClientRect();
+        const preBox = preview.getBoundingClientRect();/**lengths of image and image box based on zoom */
         const imgBox = mainImg.getBoundingClientRect();
 
 
-    
+
         const preHeight = preBox.height;
 
         const scaleHeight = imgBox.height;
-        
-        const maxUp = preHeight - scaleHeight;
+
+        const maxUp = preHeight - scaleHeight; //how long image can go up by getting a negative value
 
         if (e.key === "ArrowUp") {
             currentY -= 20;
 
-            if (currentY  < maxUp) currentY = maxUp;
+            if (currentY < maxUp) currentY = maxUp;
         }
         if (e.key === "ArrowDown") {
             currentY += 20;
@@ -185,28 +199,33 @@ function zoom() {
     })
 
 }
-
 zoom();
+
+//buttons to upload folder and image
 let add = document.querySelector(".add");
 let thumbnailBtn = document.querySelector("#thumbnail");
-  let addBox = document.querySelector(".adding");
-  let rotated = false;
+let addBox = document.querySelector(".adding");
+let rotated = false;
 
-add.addEventListener("click",(e)=>{
-    e.stopPropagation();
+//displays buttons
+add.addEventListener("click", (e) => {
+    e.stopPropagation();//prevents event handler to propagate on parent
     rotated = !rotated;
 
-    add.style.transform = rotated ? "rotate(45deg)": "rotate(0deg)";
+    add.style.transform = rotated ? "rotate(45deg)" : "rotate(0deg)";
     addBox.classList.toggle("show");
 })
-addBox.addEventListener("click",(e)=>{
+addBox.addEventListener("click", (e) => {
     e.stopPropagation();
 })
-document.body.addEventListener("click",()=>{
+
+//on body click hides upload section
+document.body.addEventListener("click", () => {
     addBox.classList.remove("show");
     add.style.transform = "rotate(0deg)"
 })
 
+//trigger events based on inputs
 let imgAdd = document.querySelector("#cam");
 let folAdd = document.querySelector("#fol");
 
@@ -217,66 +236,63 @@ imgAdd.addEventListener("click", () => {
 
 
 
- 
+
 let left = document.getElementById("left");
 let right = document.getElementById("right");
- 
 
-right.addEventListener("click",(e)=>{
+//moves the next image
+right.addEventListener("click", (e) => {
     e.preventDefault();
     left.style.opacity = 1;
     left.style.visibility = "visible";
 
-   let listImg = document.getElementsByClassName("smallImg");
-let array = Array.from(listImg);
-   const currentIndex = array.findIndex(img => img.src === mainImg.src)
-    if(currentIndex !== -1 && currentIndex < array.length - 1){
+    let listImg = document.getElementsByClassName("smallImg");
+    let array = Array.from(listImg);
+    const currentIndex = array.findIndex(img => img.src === mainImg.src)//gets the index of current image
+    if (currentIndex !== -1 && currentIndex < array.length - 1) {
         const nextImg = array[currentIndex + 1];
-        mainImg.src = nextImg.src ;
+        mainImg.src = nextImg.src;
 
     }
-    if(currentIndex + 1  === array.length - 1){
+    if (currentIndex + 1 === array.length - 1) {
         right.style.opacity = 0;
         right.style.visibility = "hidden";
-        
+
     }
 })
-left.addEventListener("click",(e)=>{
+
+//gets the previous image
+left.addEventListener("click", (e) => {
     let listImg = document.getElementsByClassName("smallImg");
     let array = Array.from(listImg);
     right.style.opacity = 1;
     right.style.visibility = "visible";
-    
-    const currentIndex = array.findIndex(img => img.src === mainImg.src);
-    if(currentIndex > 0){
+
+    const currentIndex = array.findIndex(img => img.src === mainImg.src);//same thing
+    if (currentIndex > 0) {
         const preImg = array[currentIndex - 1];
         mainImg.src = preImg.src;
-        
+
     }
-    if(currentIndex - 1 === 0){
+    if (currentIndex - 1 === 0) {
         left.style.opacity = 0;
         left.style.visibility = "hidden";
     }
 })
 
-// let listImg = document.getElementsByClassName("smallImg");
-// let array = Array.from(listImg);
-// if(array[0].src = mainImg.src){
-//     left.style.display = "none";
-// }
-
-document.body.addEventListener("keydown",(e)=>{
-    if(e.ctrlKey && e.key === "i"){
+//triggers events on keyborad keys
+document.body.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "i") {
         input.click();
     }
-    if(e.ctrlKey && e.key === "f"){
+    if (e.ctrlKey && e.key === "f") {
         folderInput.click();
     }
-    if(e.key === "ArrowRight"){
+    if (e.key === "ArrowRight") {
         right.click();
         // right();
     }
-    if(e.key === "ArrowLeft"){
+    if (e.key === "ArrowLeft") {
         left.click();
     }
 })
