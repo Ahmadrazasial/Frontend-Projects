063c70d5b5fcd1ccd79d6cda0d116130dfb206f6
Ahmadrazasial <ahmadrazauch15@gmail.com>
2025-06-24 23:05:36 +0500

    update

--- Spotify-Clone/script.js
+++ Spotify-Clone/script.js
@@ -1,5 +1,6 @@
 console.log("working");
 let currentSong = new Audio();
+// let start = document.querySelectorAll(".start");
 async function getsongs() {
     try {
         let a = await fetch("http://127.0.0.1:5500/spotify-clone/songs/")
@@ -27,15 +28,29 @@ async function getsongs() {
 }
 
 
-const playMusic = (track)=>{
+const playMusic = (track,pause=false)=>{
     currentSong.src = "/spotify-clone/songs/" + track;
+if(!pause){
     currentSong.play();
+    play.src = "pause.svg";
+    // start.src = "pause.svg";
+}
+    document.querySelector(".songinfo").innerHTML = decodeURI(track);
+    document.querySelector(".songtime").innerHTML =" 00:00/00:00";
 }
 
-async function main() {
-    
 
+function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+
+async function main() {
 let songs = await getsongs();
+let firstTrack = songs[0].split("/songs/")[1]
+playMusic(firstTrack,pause=true);
 
 let SongUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
 for (const song of songs) {
@@ -61,14 +76,15 @@ SongUl.innerHTML =  SongUl.innerHTML + `
                                     PlayNow
 
                                 </span>
-                                <img class="filter" src="play.svg" alt="play">
+                                <img class="filter" id= "start" src="play.svg" alt="play">
                             </div>
                         </li>
 
 
 `;    
-
+// playMusic(song,true);
 }
+//play songs by clicking on it
 Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
     console.log(e);
     e.addEventListener("click",element=>{
@@ -84,7 +100,35 @@ Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEa
     })
 
 })
+//play and pause song with song buttons
 
+play.addEventListener("click",()=>{
+    if(currentSong.paused){
+        currentSong.play();
+        play.src = "pause.svg"
+        
+    }else{
+        currentSong.pause();
+        play.src = "play.svg";
+    }
+})
+
+//attach eventlistner to on time update
+
+currentSong.addEventListener("timeupdate",()=>{
+    document.querySelector(".songtime").innerHTML = `${formatTime(currentSong.currentTime)}/${formatTime(currentSong.duration)}`
+    console.log(currentSong.currentTime,currentSong.duration);
+    document.querySelector(".circle").style.left = ((currentSong.currentTime/currentSong.duration)* 100) +"%";
+})
+
+//attach event listner to seekbar
+
+document.querySelector(".seekbar").addEventListener("click",e=>{
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    console.log(percent);
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = ((currentSong.duration) * percent)/100
+})
 
 }
 main();
